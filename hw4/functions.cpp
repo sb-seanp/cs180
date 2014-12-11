8058e8d8151fde2a94f6f26e2a6dddc952217ab4
Sean Prasertsit <sean@prasertsit.com>
2014-12-10 20:46:08 -0800

    Actually added file and finished scale

---
+++ hw4/functions.cpp
@@ -0,0 +1,87 @@
+// Sean Prasertsit, 10-24-2014
+
+#define _USE_MATH_DEFINES
+
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <cmath>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+vector<vector<float> > mymatrix(4, vector<float>(4));
+
+stack<vector<vector<float> > > mystack;
+
+void PushMatrix(){
+  vector<vector<float> > copy = mystack.top();
+  mystack.push(copy);
+}
+
+void PopMatrix(){
+  mystack.pop();
+}
+
+void Rotatef(float angle, float x, float y, float z){
+	vector<vector<float> > matrix = mystack.top();
+
+	float a = angle*M_PI/180.0;
+	float s = sin(a);
+	float c = cos(a);
+	float t = 1.0 - c;
+	
+	float tx = t * x;
+	float ty = t * y;
+	float tz = t * z;
+	
+	float sz = s * z;
+	float sy = s * y;
+	float sx = s * x;
+	
+	matrix[0][0]  = tx * x + c;
+	matrix[0][1]  = tx * y + sz;
+	matrix[0][2]  = tx * z - sy;
+	matrix[0][3]  = 0;
+
+	matrix[1][0]  = tx * y - sz;
+	matrix[1][1]  = ty * y + c;
+	matrix[1][2]  = ty * z + sx;
+	matrix[1][3]  = 0;
+
+	matrix[2][0]  = tx * z + sy;
+	matrix[2][1]  = ty * z - sx;
+	matrix[2][2] = tz * z + c;
+	matrix[2][3] = 0;
+
+	matrix[3][0] = 0;
+	matrix[3][1] = 0; 
+	matrix[3][2] = 0;
+	matrix[3][3] = 1; 
+}
+
+void Translatef(float x, float y, float z){
+	vector<vector<float> > matrix = mystack.top();
+	
+	matrix[0][-1] += matrix[0][0]*x + matrix[0][1]*y + matrix[0][2]*z;
+	matrix[1][-1] += matrix[1][0]*x + matrix[1][1]*y + matrix[1][2]*z;
+	matrix[2][-1] += matrix[2][0]*x + matrix[2][1]*y + matrix[2][2]*z;
+	matrix[3][-1] += matrix[3][0]*x + matrix[3][1]*y + matrix[3][2]*z;
+}
+
+void Scalef(float x, float y, float z){
+	vector<vector<float> > matrix = mystack.top();
+	
+	matrix[0][0] *= x; 
+	matrix[1][1] *= y;
+	matrix[2][2] *= z;
+}
+
+int main(int argc, char **argv){
+	for(int t = 0; t < 4; t++)
+    mymatrix[t][t] = 1;
+	for(int i = 0; i < 4; i++)
+		cout << mymatrix[i][i];
+	return 0;
+}
